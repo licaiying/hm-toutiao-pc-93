e85e8e7b8f8b8459c8550f3845cea3944f6e3c67
licaiying <1194449590@qq.com>
2020-02-06 19:34:08 +0800

    项目配置了element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    
+    <el-button type="success" disabled>成功按钮</el-button>
+    <el-button type="info" icon="el-icon-message" circle></el-button>
   </div>
 </template>
 
