b4f2cbdebc4f1bfd7cb62537f78ab7d77864d5b4
licaiying <1194449590@qq.com>
2020-02-06 21:47:07 +0800

    登录模块-校验-表单元素

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,13 +2,13 @@
   <div class="container-login">
     <el-card class="my-card">
       <img src="../../assets/logo_index.png" alt />
-      <el-form :model="loginForm">
+      <el-form :model="loginForm" :rules="loginRules" status-icon>
         <!-- 手机号 -->
-        <el-form-item>
+        <el-form-item prop="mobile">
           <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
         <!-- 验证码 -->
-        <el-form-item>
+        <el-form-item prop="code">
           <el-input
             v-model="loginForm.code"
             placeholder="请输入验证码"
@@ -33,10 +33,37 @@
 export default {
   name: "app-login",
   data() {
+    const checkMobile = (rule, value, callback) => {
+      // 手机号逻辑 (以1开头  第二位：3-9的数字  其余剩下9位数字)
+      if (!/^1[3-9]\d{9}$/.test(value)) {
+        // 校验失败
+        callback(new Error("手机号格式不正确"));
+      } else {
+        // 校验成功
+        callback();
+      }
+    };
     return {
+      // 表单对应的数据对象
       loginForm: {
         mobile: "",
         code: ""
+      },
+      // 对单对应的校验规则
+      loginRules: {
+        mobile: [
+          // required 是否必填
+          // message 提示信息
+          // trigger 触发校验的时机
+          { required: true, message: "请输入手机号", trigger: "blur" },
+          // 自定义校验手机号  通过 validator 进行声明校验 固定写法
+          { validator: checkMobile, trigger: "blur" }
+        ],
+        code: [
+          { required: true, message: "请输入验证码", trigger: "blur" },
+          // len 验证码的长度
+          { len: 6, message: "验证码为6位数字", trigger: "blur" }
+        ]
       }
     };
   }
