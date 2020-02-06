40acfb89dd0b044b935ed9c6410c34af3d9824e0
licaiying <1194449590@qq.com>
2020-02-06 19:47:58 +0800

    项目配置了路由vue-router

--- src/main.js
+++ src/main.js
@@ -5,10 +5,14 @@ import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+// 引入路由
+import router from './router/index.js'
+
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
