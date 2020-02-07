eafd29bffc90420e9861d520aa97dbabca719dac
licaiying <1194449590@qq.com>
2020-02-07 21:17:22 +0800

    首页模块-基础布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="container-home">home</div>
+  <div class="container-home">
+    <el-container class="container-home">
+      <el-aside class="my-aside" width="200px">Aside</el-aside>
+      <el-container>
+        <el-header class="my-header">Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script type="text/javascript">
@@ -9,4 +17,17 @@ export default {
 </script>
 
 <style scoped lang="less" >
+.container-home {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  .my-aside {
+    background: #002233;
+  }
+  .my-header {
+    border-bottom: 1px solid #ddd;
+  }
+}
 </style>
