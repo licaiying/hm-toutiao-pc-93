5e09bb20173bcf1cb70b1923b99efdeb71427542
licaiying <1194449590@qq.com>
2020-02-07 21:01:29 +0800

    首页模块-路由及组件

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,15 @@ import VueRouter from 'vue-router'
 // 导入Login
 import Login from '@/views/login'
 
+// 导入Home
+import Home from '@/views/home'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
     routes: [
-        { path: '/login', component: Login }
+        { path: '/login', component: Login },
+        { path: '/', component: Home }
     ]
 });
 
---
+++ src/views/home/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container-home">home</div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: "app-home"
+};
+</script>
+
+<style scoped lang="less" >
+</style>
