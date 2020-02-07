ee7cf5da3b4160660e68577dc23d8e761a186e57
licaiying <1194449590@qq.com>
2020-02-07 21:47:36 +0800

    首页模块-头部导航

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,7 +3,24 @@
     <el-container class="container-home">
       <el-aside class="my-aside" width="200px">Aside</el-aside>
       <el-container>
-        <el-header class="my-header">Header</el-header>
+        <el-header class="my-header">
+          <span class="icon el-icon-s-fold"></span>
+          <span class="text">江苏传智播客科技教育有限公司</span>
+          <!-- 右侧下拉菜单 -->
+          <el-dropdown class="my-dropdown">
+            <span class="el-dropdown-link">
+              <!-- 用户头像 -->
+              <img class="head" src="../../assets/avatar.jpg" alt />
+              <!-- 用户名称 -->
+              <strong class="name">黄渤</strong>
+              <i class="el-icon-arrow-down el-icon--right"></i>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>个人设置</el-dropdown-item>
+              <el-dropdown-item>退出登录</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-header>
         <el-main>Main</el-main>
       </el-container>
     </el-container>
@@ -28,6 +45,27 @@ export default {
   }
   .my-header {
     border-bottom: 1px solid #ddd;
+    line-height: 60px;
+    .icon {
+      vertical-align: middle;
+      padding-right: 10px;
+      font-size: 24px;
+    }
+    .text {
+      vertical-align: middle;
+    }
+    .my-dropdown {
+      float: right;
+      .head {
+        width: 30px;
+        height: 30px;
+        vertical-align: middle;
+      }
+      .name {
+        vertical-align: middle;
+        padding-left: 5px;
+      }
+    }
   }
 }
 </style>
