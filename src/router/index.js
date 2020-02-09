ac34a09339f4c3e3f41258eb29dce927342a0864
licaiying <1194449590@qq.com>
2020-02-09 20:56:11 +0800

    登录补充-访问权限控制

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,9 @@ import VueRouter from 'vue-router'
 // 导入Login
 import Login from '@/views/login'
 
+// 导入工具auth(操作token的相关函数)
+import auth from '@/utils/auth'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
@@ -11,5 +14,13 @@ const router = new VueRouter({
         { path: '/login', component: Login }
     ]
 });
+
+// 设置前置导航守卫
+router.beforeEach((to, from, next) => {
+    // 如果你不是登录页面，而且你没有token，拦截到登录页面
+    if (to.path !== '/login' && !auth.getUser().token) return next('/login')
+    // 其他情况放行
+    next()
+})
 
 export default router
