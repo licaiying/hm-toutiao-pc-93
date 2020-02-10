745f2f45a56103c2d256af233da843f466c5f1a8
licaiying <1194449590@qq.com>
2020-02-10 22:33:54 +0800

    内容管理-axios响应拦截器

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 // 导出一个配置好的axios提供给main.js使用
 import axios from 'axios'
 import auth from '@/utils/auth'
+import router from '@/router'
 
 // 配置基准地址
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
@@ -16,6 +17,20 @@ axios.interceptors.request.use(config => {
     return config
 }, error => {
     return Promise.reject(error)
+})
+
+// 配置响应拦截器
+axios.interceptors.response.use(res => {
+    // 此时，不对响应的正确结果做处理
+    return res
+}, err => {
+    // err.response 是响应报文对象
+    // err.response.status 是响应的错误的状态码
+    if (err.response && err.response.status === 401) {
+        // 拦截到登录页面
+        router.push('/login')
+    }
+    return Promise.reject(err)
 })
 
 
