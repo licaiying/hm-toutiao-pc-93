81fd512f327f750ce48eb26102918f2055c5cd88
licaiying <1194449590@qq.com>
2020-02-10 21:39:08 +0800

    内容管理-axios请求拦截器

--- src/api/index.js
+++ src/api/index.js
@@ -6,7 +6,17 @@ import auth from '@/utils/auth'
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
 
 // 配置 请求头token
-axios.defaults.headers.Authorization = `Bearer ${auth.getUser().token}`
+// axios.defaults.headers.Authorization = `Bearer ${auth.getUser().token}`
+
+// 配置请求拦截器
+axios.interceptors.request.use(config => {
+    // 获取本地的token，如果有token 就追加
+    const user = auth.getUser()
+    if (user.token) config.headers.Authorization = `Bearer ${user.token}`
+    return config
+}, error => {
+    return Promise.reject(error)
+})
 
 
 // 导出axios
