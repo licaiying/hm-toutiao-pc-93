299e47090d7470018c3dd2118bca9ecb1f15c2cf
licaiying <1194449590@qq.com>
2020-02-11 21:30:37 +0800

    内容管理-筛选区域布局01-面包屑和表单结构

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="container-article">article////</div>
+  <div class="container-article">
+    <!-- 筛选区域 -->
+    <el-card>
+      <!-- 面包屑区域 -->
+      <div slot="header">
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>内容管理</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <!-- 表单区域 -->
+      <el-form label-width="80px">
+        <el-form-item label="状态"></el-form-item>
+        <el-form-item label="频道"></el-form-item>
+        <el-form-item label="日期"></el-form-item>
+        <el-form-item>
+          <el-button type="primary">筛选</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+
+    <!-- 结果区域 -->
+  </div>
 </template>
 
 <script type="text/javascript">
