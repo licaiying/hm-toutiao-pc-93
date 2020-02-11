15c0df696effb2524508991837feb69587c543c7
licaiying <1194449590@qq.com>
2020-02-11 21:52:49 +0800

    内容管理-筛选区域布局02-表单元素-状态

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -11,9 +11,18 @@
       </div>
       <!-- 表单区域 -->
       <el-form label-width="80px">
-        <el-form-item label="状态"></el-form-item>
-        <el-form-item label="频道"></el-form-item>
-        <el-form-item label="日期"></el-form-item>
+        <el-form-item label="状态：">
+          <el-radio-group v-model="filterData.status">
+            <el-radio :label="null">全部</el-radio>
+            <el-radio :label="0">草稿</el-radio>
+            <el-radio :label="1">待审核</el-radio>
+            <el-radio :label="2">审核通过</el-radio>
+            <el-radio :label="3">审核失败</el-radio>
+            <el-radio :label="4">已删除</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="频道："></el-form-item>
+        <el-form-item label="日期："></el-form-item>
         <el-form-item>
           <el-button type="primary">筛选</el-button>
         </el-form-item>
@@ -27,16 +36,26 @@
 <script type="text/javascript">
 export default {
   name: "app-article",
-  created() {
-    // 发请求进行测试
-    this.$http
-      .get("articles")
-      .then(res => {
-        console.log(res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      });
+  // created() {
+  //   // 发请求进行测试
+  //   this.$http
+  //     .get("articles")
+  //     .then(res => {
+  //       console.log(res.data);
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //     });
+  // }
+  data() {
+    return {
+      // 声明筛选条件数据，筛选条件数据提交给后台，数据的字段名称，由后台接口决定。
+      // 筛选数据是由多个表单元素组成，需要收集所有数据，应该使用对象来进行绑定
+      filterData: {
+        // 当字段的值null，axios请求不会提交，代表不传
+        status: null
+      }
+    };
   }
 };
 </script>
