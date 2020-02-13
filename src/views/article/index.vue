7295144241201af74e5b3ee2779f2a718a264e25
licaiying <1194449590@qq.com>
2020-02-13 22:27:46 +0800

    内容管理-文章列表-渲染自定义列

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -50,11 +50,37 @@
       <div slot="header">根据筛选条件共查询到 0 条结果：</div>
       <!-- 表格区域 -->
       <el-table :data="articles" style="width: 100%">
-        <el-table-column label="封面"></el-table-column>
+        <el-table-column label="封面">
+          <!-- 使用一个作用域插槽 scope 收集封装组件绑定在插槽上的所有数据 -->
+          <!-- element-ui 给插槽绑定了一个数据这个数据叫：
+          row数据，代码每一行数据(遍历数组，数组后的每一个对象)-->
+          <!-- cover:{type:'',images:[]} 取第一张图片进行预览-->
+          <template slot-scope="scope">
+            <el-image :src="scope.row.cover.images[0]" style="width:150px;height:100px">
+              <!-- 具名插槽  error  当图片加载失败使用这个插槽 -->
+              <div slot="error">
+                <img src="../../assets/error.gif" style="width:150px;height:100px" alt />
+              </div>
+            </el-image>
+          </template>
+        </el-table-column>
         <el-table-column prop="title" label="标题"></el-table-column>
-        <el-table-column label="状态"></el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.status===0" type="info">草稿</el-tag>
+            <el-tag v-if="scope.row.status===1">待审核</el-tag>
+            <el-tag v-if="scope.row.status===2" type="success">审核通过</el-tag>
+            <el-tag v-if="scope.row.status===3" type="warning">审核失败</el-tag>
+            <el-tag v-if="scope.row.status===4" type="danger">已删除</el-tag>
+          </template>
+        </el-table-column>
         <el-table-column prop="pubdate" label="发布时间"></el-table-column>
-        <el-table-column label="操作"></el-table-column>
+        <el-table-column label="操作" width="120px"> 
+          <template>
+            <el-button plain type="primary" icon="el-icon-edit" circle></el-button>
+            <el-button plain type="danger" icon="el-icon-delete" circle></el-button>
+          </template>
+        </el-table-column>
       </el-table>
       <!-- 分页区域 -->
       <el-pagination background layout="prev, pager, next" :total="1000"></el-pagination>
