85a602d09e4c11ddde02d848a5f22767edd9dc1d
licaiying <1194449590@qq.com>
2020-02-15 01:45:53 +0800

    发布文章-基础布局

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,10 +1,38 @@
 <template>
-  <div class="container-publish">publish000</div>
+  <div class="container-publish">
+    <el-card>
+      <div slot="header">
+        <!-- 使用自己封装的面包屑组件 -->
+        <my-bread>发布文章</my-bread>
+      </div>
+      <!-- 表单区域 -->
+      <el-form label-width="120px">
+        <el-form-item label="标题：">
+          <el-input v-model="articleForm.title" style="width:400px"></el-input>
+        </el-form-item>
+        <el-form-item label="内容：">富文本</el-form-item>
+        <el-form-item label="封面：">封面组件</el-form-item>
+        <el-form-item label="频道：">频道组件</el-form-item>
+        <el-form-item>
+          <el-button type="primary">发布文章</el-button>
+          <el-button>存入草稿</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script type="text/javascript">
 export default {
-  name: "app-publish"
+  name: "app-publish",
+  data() {
+    return {
+      // 文章数据
+      articleForm: {
+        title: null
+      }
+    };
+  }
 };
 </script>
 
