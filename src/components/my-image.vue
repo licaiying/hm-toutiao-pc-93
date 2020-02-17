d5c15c06cdc6bf06045c241b4d0cd15df92ee796
licaiying <1194449590@qq.com>
2020-02-17 20:44:13 +0800

    发布文章-封面组件-tab组件和基础布局

--- src/components/my-image.vue
+++ src/components/my-image.vue
@@ -6,7 +6,24 @@
     </div>
     <!-- 对话框 -->
     <el-dialog :visible.sync="dialogVisible" width="750px">
-      <span>tab组件</span>
+      <el-tabs v-model="activeName" type="card">
+        <el-tab-pane label="素材库" name="list">
+          <!-- 按钮 -->
+          <el-radio-group v-model="reqParams.collect" size="small">
+            <el-radio-button :label="false">全部</el-radio-button>
+            <el-radio-button :label="true">收藏</el-radio-button>
+          </el-radio-group>
+          <!-- 图片列表 -->
+          <div class="img-list">
+            <div class="img-item" v-for="i in 8" :key="i">
+              <img src="../assets/avatar.jpg" alt />
+            </div>
+          </div>
+          <!-- 分页 -->
+          <el-pagination background layout="prev, pager, next" :total="1000"></el-pagination>
+        </el-tab-pane>
+        <el-tab-pane label="上传图片" name="upload">2</el-tab-pane>
+      </el-tabs>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
         <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
@@ -20,7 +37,14 @@ export default {
   name: "my-image",
   data() {
     return {
-      dialogVisible: false
+      // 获取素材列表参数对象
+      reqParams: {
+        collect: false,
+        page: 1,
+        per_page: 8
+      },
+      dialogVisible: false,
+      activeName: "list"
     };
   },
   methods: {
@@ -36,6 +60,7 @@ export default {
   display: inline-block;
   margin-right: 20px;
 }
+// 图片按钮
 .img-btn {
   width: 150px;
   height: 150px;
@@ -44,6 +69,22 @@ export default {
     width: 100%;
     height: 100%;
     display: block;
+  }
+}
+// 图片列表
+.img-list {
+  padding-top: 15px;
+  .img-item {
+    width: 150px;
+    height: 120px;
+    border: 1px dashed #ddd;
+    display: inline-block;
+    margin-right: 20px;
+    img {
+      width: 100%;
+      height: 100%;
+      display: block;
+    }
   }
 }
 </style>
