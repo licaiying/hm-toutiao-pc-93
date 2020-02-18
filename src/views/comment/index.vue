df77a26db3b2720ea9044ca775aee3b9e077c351
licaiying <1194449590@qq.com>
2020-02-19 00:37:08 +0800

    评论管理-基础布局

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,10 +1,33 @@
 <template>
-  <div class="container-comment">comment</div>
+  <div class="container-comment">
+    <el-card>
+      <!-- 面包屑 -->
+      <div slot="header">
+        <my-bread>评论管理</my-bread>
+      </div>
+      <!-- 表格 -->
+      <el-table :data="comments">
+        <el-table-column label="文章标题"></el-table-column>
+        <el-table-column label="总评论数"></el-table-column>
+        <el-table-column label="粉丝评论数"></el-table-column>
+        <el-table-column label="评论状态"></el-table-column>
+        <el-table-column label="操作"></el-table-column>
+      </el-table>
+      <!-- 分页 -->
+      <el-pagination style="margin-top:20px" background layout="prev, pager, next" :total="1000"></el-pagination>
+    </el-card>
+  </div>
 </template>
 
 <script type="text/javascript">
 export default {
-  name: "app-comment"
+  name: "app-comment",
+  data() {
+    return {
+      // 评论列表（文章列表，此时用来维护评论功能）
+      comments: []
+    };
+  }
 };
 </script>
 
